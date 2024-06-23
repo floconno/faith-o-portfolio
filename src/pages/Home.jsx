1a41460675513e5f374a667d8d5f163012bfac17
floconno <floconno@asu.edu>
2024-06-23 14:16:45 -0700

    added styling and extra button to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../App.css';
+import HomeHero from '../components/HomeHero';
 
 
 
 const Home = () => {
+    return (
+        <div>
+        <HomeHero/>
+    </div>
+    )
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
   content: [
-    './src/**/*.{js,jsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
