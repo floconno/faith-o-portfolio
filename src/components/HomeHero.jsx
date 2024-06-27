06225ab99074dda96562f8edcb436f19add86664
floconno <floconno@asu.edu>
2024-06-27 00:55:08 -0700

    added start script to package.json

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -9,7 +9,7 @@ const HomeHero = () => {
           <p className="py-6">
             Thank you for taking the time to view my portfolio! Please browse my projects and the rest of my site. Below are links to view my resume and GitHub!
           </p>
-          <a className="btn btn-primary m-5" href='https://docs.google.com/document/d/1BihOwOeDVsYWpfUOpa2GJQ555Tdubr-XzQC--uZtKgU/edit?usp=sharing' target='_blank' rel='noopener noreferrer'>View my resume</a>
+          <a className="btn btn-primary m-5" href='https://docs.google.com/document/d/1BihOwOeDVsYWpfUOpa2GJQ555Tdubr-XzQC--uZtKgU/edit?usp=sharing' target='_blank' rel='noopener noreferrer'>View my resume!</a>
           <a className="btn btn-primary" href='https://github.com/floconno' target='_blank' rel='noopener noreferrer'>View my GitHub!</a>
         </div>
       </div>
