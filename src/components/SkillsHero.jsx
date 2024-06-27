7cb0c3c6435083e4741a43da023d72bca237ef1c
floconno <floconno@asu.edu>
2024-06-26 23:45:29 -0700

    added skills icons

--- src/components/SkillsHero.jsx
+++ src/components/SkillsHero.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../fontAwesome.js'; 
+import { SiMongodb, SiExpress, SiMysql } from "react-icons/si";
 
 const Skills = () => {
 
     const skillsIcons = [
-        { name: 'JavaScript', icon: ['fab', 'js'], color:'text-blue-200 hover:text-blue-700' },
-        { name: 'React', icon: ['fab', 'react'], color:'text-blue-200 hover:text-blue-700' }
+        { name: 'JavaScript', icon: ['fab', 'js'], color:'text-blue-200 hover:text-blue-700', type: 'font-awesome' },
+        { name: 'React', icon: ['fab', 'react'], color:'text-blue-200 hover:text-blue-700', type: 'font-awesome' },
+        { name: 'HTML', icon: ['fab', 'html5'], color:'text-blue-200 hover:text-blue-700', type: 'font-awesome' },
+        { name: 'CSS', icon: ['fab', 'css3-alt'], color:'text-blue-200 hover:text-blue-700', type: 'font-awesome' },
+        { name: 'Node.js', icon: ['fab', 'node'], color:'text-blue-200 hover:text-blue-700', type: 'font-awesome' },
+        { name: 'MongoDB', icon: SiMongodb, color:'text-blue-200 hover:text-blue-700', type: 'react-icons' },
+        { name: 'Express.js', icon: SiExpress, color:'text-blue-200 hover:text-blue-700', type: 'react-icons' },
+        { name: 'MySQL', icon: SiMysql, color:'text-blue-200 hover:text-blue-700', type: 'react-icons' },
+        { name: 'Git', icon: ['fab', 'git-alt'], color:'text-blue-200 hover:text-blue-700', type: 'font-awesome' },
+
     ];
 
     const renderSkillsIcons = () => {
-        return skillsIcons.map((skillsIcons, index) => (
+        return skillsIcons.map((skillsIcon, index) => (
           <div key={index} className="flex flex-col items-center mx-2">
-            <a key={index} href={skillsIcons.link} target='_blank' rel='noopener noreferrer'>
-                <FontAwesomeIcon icon={skillsIcons.icon} className={`h-20 w-20 ${skillsIcons.color} mx-2`} />
+            <a key={index} href={skillsIcon.link} target='_blank' rel='noopener noreferrer'>
+              {skillsIcon.type === 'font-awesome' ? (
+                <FontAwesomeIcon icon={skillsIcon.icon} className={`h-20 w-20 ${skillsIcon.color} mx-2`} />
+              ) : (
+                <skillsIcon.icon className={`h-20 w-20 ${skillsIcon.color} mx-2`} />
+              )}
             </a>
-            <span className="mt-2 text-base">{skillsIcons.name}</span>
+            <span className="mt-2 text-base">{skillsIcon.name}</span>
             </div>
         ));
     };
--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,4 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faLinkedin, faReact, faJs } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faReact, faJs, faHtml5, faCss3Alt, faNode, faGitAlt } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faGithub, faLinkedin, faReact, faJs);
+
+library.add(faGithub, faLinkedin, faReact, faJs, faHtml5, faCss3Alt, faNode, faGitAlt);
