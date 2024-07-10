ab93218371edcbc815cde3aeb6767d92997cf698
floconno <floconno@asu.edu>
2024-07-09 20:58:17 -0700

    fixing bugs after deployment

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
 
 dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(express.static('public'));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -42,7 +42,7 @@ app.post('/send', (req, res) => {
   });
 
   app.get('/', (req, res) => {
-    res.send('Hello, this is your Express server running on Heroku.');
+    res.send('Hello, this is your Express server running on Vercel.');
   });
   
   
