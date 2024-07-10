ad0adeca48699bad2f5ef2f56c61f2e7aea1ac14
floconno <floconno@asu.edu>
2024-07-09 21:15:57 -0700

    fixing bugs

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.static('public'));
+
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
